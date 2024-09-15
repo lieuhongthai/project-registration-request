a063f0be15881cdfdb29e919dc6a6cfe7fc0645b
lieuhongthai <69983449+lieuhongthai@users.noreply.github.com>
2024-09-15 22:17:46 +0700

    Update Babel configuration and package.json dependencies

--- client/src/pages/user-management/edit/index.tsx
+++ client/src/pages/user-management/edit/index.tsx
@@ -2,6 +2,7 @@ import { useParams } from 'react-router-dom';
 
 const UserEdit = () => {
   const { id: userId } = useParams<{ id: string }>();
+
   return <div>UserEdit - User id: {userId}</div>;
 };
 
